3778be59587d811dab75fd879ffb153ec3d6c2d6
Farid <71142299+Mr-Farid@users.noreply.github.com>
2021-02-10 20:01:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Exercise x.x
+## Instructions
+
+Go to the [exercise file](exercise.py) and complete the question below.
+
+Create a program that...
+## Specifications
+The input will be...
+
+Make sure the output is exactly what is described! The output must be...
+## Examples
+Sample Input:
+```Python
+stuff
+```
+Sample Output:
+```Python
+stuff
+```
+## Run Test
+Ensure your program passes all the tests. 
+
+if pytest is not installed, use command:
+```
+pip3 install pytest
+```
+Run test using the following command in the correct folder in terminal:
+```
+pytest -vv
+```
---
+++ exercise.py
@@ -0,0 +1,20 @@
+# detailed prompts, prints, and comments not needed for pytest
+
+def main():
+    # write your code below this line
+    # make sure its indented like the sample code below
+    # replace the sample addition algorithm below 
+    x = int(input(''))
+    y = int(input(''))
+    sum = x + y
+    print(sum)
+
+
+#########################################################################
+#################### Do not modify this block ###########################
+
+if __name__ == '__main__':
+    main()
+
+#################### Do not modify this block ###########################
+#########################################################################
